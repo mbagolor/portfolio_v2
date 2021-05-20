2b97303c49d9a53f7996d3ea992e38c9b5d86f15
mbagolor <mbagolor@hawaii.edu>
2021-05-19 21:15:53 -1000

    \scripts js

--- js/script.js
+++ js/script.js
@@ -8,3 +8,30 @@ mymenubutton.onclick = function() {
         mysitenav.setAttribute("data-navstate", "open");
     }
 };
+
+// INTERSECTION OBSERVER
+let options = {
+    rootMargin: '0px',
+    threshold: 0.2
+}
+
+let callback = (entries, observer) => {
+    entries.forEach(entry => {
+        let elem = entry.target;
+
+        if (entry.isIntersecting) {
+            elem.setAttribute('data-view', 'visible');
+        } else {
+            elem.setAttribute('data-view', 'hidden');
+        }
+    });
+};
+
+let observer = new IntersectionObserver(callback, options);
+
+
+let targets = document.querySelectorAll('.scroll-element');
+
+targets.forEach(target => {
+    observer.observe(target);
+})
